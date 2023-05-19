fbc975a37d0ad6c6856cb1077f476d217f906097
Karthik-beta <karthikpar@protonmail.com>
2023-05-19 09:42:44 +0530

    0.5.0
    Only backend is added and it contains authentication system

---
+++ backend/authApp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None
+    is_superuser_access = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
---
+++ backend/authApp/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from .serializers import UserSerializer
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+from .models import User
+import jwt, datetime
+
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+
+
+class LoginView(APIView):
+    permission_classes = [IsAuthenticated] 
+
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = authenticate(email=email, password=password)
+
+        if user is None:
+            raise AuthenticationFailed('Invalid credentials, try again')
+            
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+        
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow(),
+            'is_superuser': user.is_superuser,
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        
+        if user.is_superuser:
+            response.data = {
+                'message': 'Logged in as superuser',
+                'jwt': token
+            }
+        else:
+            response.data = {
+                'message': 'Logged in as regular user',
+                'jwt': token
+            }
+        
+        return response
+    
+
+class UserView(APIView):
+
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message': 'success'
+        }
+        return response
